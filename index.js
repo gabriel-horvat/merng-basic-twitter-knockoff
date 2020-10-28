1a861a1791c3dd527b9557d1325c18081234b58e
Gabmeister <gabriel@gabrielhorvat.com>
2020-10-28 16:48:23 +0100

    Post model

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const {ApolloServer} = require('apollo-server')
 const gql = require('graphql-tag')
 const mongoose = require('mongoose')
+
+
 const {MONGODB} = require('./config.js')
+const Post = require('./models/Post')
 
 const typeDefs = gql`
 type Query {
-    sayHi: String!
+    getPosts
 }
 `
 
