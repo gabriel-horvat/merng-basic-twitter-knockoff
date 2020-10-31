c7b033e3cc3535f640bf9dc21166a43035cb9741
Gabmeister <gabriel@gabrielhorvat.com>
2020-10-31 21:53:27 +0100

    likePost

--- graphQL/resolvers/posts.js
+++ graphQL/resolvers/posts.js
@@ -1,4 +1,4 @@
-const {AuthenticationError} = require('apollo-server')
+const {AuthenticationError, UserInputError} = require('apollo-server')
 
 const Post = require('../../models/Post')
 const auth = require('../../utils/auth')
@@ -60,5 +60,25 @@ throw new AuthenticationError('Action not permitted!')
         } catch (err) {
             throw new Error(err)
         }
+
+    },
+
+    async likePost(_, {postId}, context){
+        const {username} = auth(context)
+        const post = await Post.findById(postId)
+        if (post) {
+            if (post.likes.find(like => like.username === username)) {
+                post.likes = post.likes.filter(like => like.username !== username)
+            }
+            else{
+                post.likes.push({
+                    username,
+                    createdAt: new Date().toISOString()
+                })
+            }
+            await post.save()
+            return post
+        } else throw new UserInputError('post not found')
+        
     }
 }
